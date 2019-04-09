7c8c3dae891898a5456821db2edee854653b7ade
Peace Han <peacehan@gmail.com>
2019-04-09 16:42:43 -0400

    datasets added

---
+++ extractRelations.py
@@ -0,0 +1,13 @@
+'''
+This is a program to pre-process relation extraction data
+
+feature vector:
+[
+shortest dependency path - raw
+  shortest dependency path - POS
+  distance to main verb/root
+]
+
+@author Peace Han
+'''
+
